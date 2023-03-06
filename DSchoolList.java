df59f2b3dfb12073fe1516e5ac389844aa9c646c
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2023-03-06 21:15:58 +0800

    Create DSchoolList.java

---
+++ DSchoolList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class DSchoolList {
+    // data section
+    ArrayList<School> schools = new ArrayList<>();
+
+    public void addSchool(School sc) {
+        schools.add(sc);
+    }
+
+    public void display() {
+        for (int i=0; i<schools.size(); i++) {
+            System.out.print(schools.get(i));
+        }
+    }
+
+}
